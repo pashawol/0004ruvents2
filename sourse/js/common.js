0ccd9008b50014082033d1b073b0c940dfd542b5
MaxSimakov <Simakov.maxim82@gmail.com>
2021-04-11 15:18:44 +0300

    finished offline.html

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -294,12 +294,17 @@ function eventHandler() {
 
 	var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
 	if (isIE11) {
-		$("body").prepend(`<p   class="browsehappy container">К сожалению, вы используете устаревший браузер. Пожалуйста, <a href="http://browsehappy.com/" target="_blank">обновите ваш браузер</a>, чтобы улучшить производительность, качество отображаемого материала и повысить безопасность.</p>`)
+		$("body").prepend(`<p class="browsehappy container">К сожалению, вы используете устаревший браузер. Пожалуйста, <a href="http://browsehappy.com/" target="_blank">обновите ваш браузер</a>, чтобы улучшить производительность, качество отображаемого материала и повысить безопасность.</p>`)
 
 	}
 	const sPhotoSlider = new Swiper('.sPhoto__slider--js', {
 		// Optional parameters
 		loop: true,
+		slidesPerView: 'auto',
+		lazy: {
+			loadPrevNext: true,
+			loadPrevNextAmount: 6,
+		},
 	
 		// If we need pagination
 		// pagination: {
